dbd00da21620d3021f9d09d870a69c9bd32120f5
Nidhi Dahiya <bisenlakshya005@gmail.com>
2025-07-22 10:09:15 +0530

    generate.py

---
+++ generate.py
@@ -0,0 +1,16 @@
+import random
+import string
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = " ".join(random.choice(characters) for _ in range(length))
+   
+    return password
+print("Random Password Generator")
+try:
+    length = int(input("enter desired password length:"))
+    if length<4:
+        print("Password should be atleast 4 characters long")
+    else:
+        print("Generated Password",generate_password(length))
+except ValueError:
+    print("Please enter a valid number")
